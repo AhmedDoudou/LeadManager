d84c032c3579a4bea810bbe1923bb9284767998c
AhmedDoudou <ahmeddoudou.id@gmail.com>
2022-03-05 08:22:03 +0100

    initial commit

---
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Lead Manager</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Home
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Store</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header 
